f6d2a3074f9d2f2bd297415c8f6252f725354ce3
Prakarsh Verma <prakarsh1248@gmail.com>
2024-11-06 00:53:15 +0530

    Created Header

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -5,7 +5,8 @@ function Header() {
     <div className='h-40 w-full flex items-center justify-between'>
         <div className='flex gap-6 ml-20'>
             <img src="logo.svg" alt="" />
-            <span className='text-[#172B4D] font-bold'>AlgoZenith</span>
+            <img src="AlgoZenithLogo.svg" alt="" />
+            {/* <span className='text-[#172B4D] font-bold'>AlgoZenith</span> */}
         </div>
         <div className='w-36 flex justify-between items-center mr-16'>
             <div className='flex justify-center items-center p-3 bg-[#E7F8FF]'>
